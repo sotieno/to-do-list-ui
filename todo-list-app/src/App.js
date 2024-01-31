fe4ef4e5e7e61cc8d8bbf1f8205b2063d23389aa
Sylvia Otieno <sylvia.a.otieno@gmail.com>
2024-01-31 17:56:23 +0300

    Update App.js

--- todo-list-app/src/App.js
+++ todo-list-app/src/App.js
@@ -1,13 +1,20 @@
+// src/App.js
+
 import './App.css';
 import React, { useState } from 'react';
-import TodoList from './components/TodoList';
-import AddTodo from './components/AddTodo';
+import ToDoItem from './components/ToDoItem';
+import AddToDo from './components/AddToDo';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   const [todos, setTodos] = useState([]);
 
-  const handleAddTodo = (text) => {
-    setTodos([...todos, { id: Date.now(), text }]);
+  const handleAddTodo = (owner, task, taskStatus) => {
+    setTodos([...todos, { id: Date.now(), owner, task, taskStatus }]);
+  };
+
+  const handleEditTodo = (id, editedStatus) => {
+    setTodos(todos.map(todo => (todo.id === id ? { ...todo, taskStatus: editedStatus } : todo)));
   };
 
   const handleDeleteTodo = (id) => {
@@ -17,10 +24,29 @@ const App = () => {
   return (
     <div className="container mt-5">
       <h1 className="mb-4">Todo List</h1>
-      <AddTodo onAdd={handleAddTodo} />
-      <TodoList todos={todos} onDelete={handleDeleteTodo} />
+      <AddToDo onAdd={handleAddTodo} />
+      <table className="table mt-3">
+        <thead>
+          <tr>
+            <th>Owner</th>
+            <th>Task</th>
+            <th>Status</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {todos.map(todo => (
+            <ToDoItem
+              key={todo.id}
+              todo={todo}
+              onDelete={handleDeleteTodo}
+              onEdit={handleEditTodo}
+            />
+          ))}
+        </tbody>
+      </table>
     </div>
   );
-}
+};
 
 export default App;
